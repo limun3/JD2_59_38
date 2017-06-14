215f1f2ae681ccda19e80d6256f31f58fb7af40e
limun3 <nkanjeric@gmail.com>
2017-06-14 11:47:34 +0200

    servis za login i auth

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-// import { LoginService } from './login.service'
+import { LoginService } from './login.service'
 import { LoginData } from './login-data.model'
-// import { AuthService } from '../services/auth.service';
+import { AuthenticationService } from '../services/authentication.service';
 import {
   Router,
   ActivatedRoute
@@ -11,31 +11,26 @@ import {
   selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-//   providers: [LoginService, AuthService]
+  providers: [LoginService, AuthenticationService]
 })
 export class LoginComponent implements OnInit {
   Username: string;
   Password: string;
 
-//   constructor(private loginService: LoginService, private authService: AuthService, private router: Router) {
-//    }
-    constructor(){
-        
-    }
+  constructor(private loginService: LoginService, private authService: AuthenticationService, private router: Router) {
+   }
+
   ngOnInit() {
-    /*if (this.authService.isLoggedIn()){
+    if (this.authService.isLoggedIn()){
       this.router.navigate(['/home']);
-    }*/
+    }
   }
 
   onSubmit() {
 
-    // let userName = this.Username;
-    // this.loginService.login(new LoginData(this.Username, this.Password)).subscribe(x => {this.authService.logIn(x); 
-    //     this.router.navigate(['/']);});
-
-    //this.Username = "";
-    //this.Password = "";
+    let userName = this.Username;
+    this.loginService.login(new LoginData(this.Username, this.Password)).subscribe(x => {this.authService.logIn(x); 
+        this.router.navigate(['/']);});
   }
 
 }
